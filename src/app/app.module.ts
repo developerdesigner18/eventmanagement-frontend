6c56887f592d283bf16cba4657f1c1922c1044b5
developer.designer18@gmail.com <developer.designer18@gmail.com>
2021-09-27 10:54:19 +0530

    component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { GalaryComponent } from './components/galary/galary.component';
+import { EventsComponent } from './components/events/events.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactUsComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    BlogsComponent,
+    GalaryComponent,
+    EventsComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
